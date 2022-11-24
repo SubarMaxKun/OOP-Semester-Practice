364e1a2c52acbcd9c8aae94931a247b5024bc7c6
SubarMaxKun <maxgamer0027@gmail.com>
2022-11-24 13:25:45 +0200

    Added functionality for Registration

--- src/com/nekonyan/authentication/Registration.java
+++ src/com/nekonyan/authentication/Registration.java
@@ -1,7 +1,36 @@
 package com.nekonyan.authentication;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Registration {
-  public void registration(){
-    System.out.println("registration");
+  Scanner scanner = new Scanner(System.in);
+  File file = new File("./src/com/nekonyan/resources/Users.txt");
+
+  public void registration() throws IOException {
+    boolean exist = file.exists();
+    writeToFile(exist);
+  }
+
+  private void writeToFile(boolean append) throws IOException{
+    String username;
+    String password;
+
+    System.out.println("Введіть логін:");
+    username = scanner.nextLine();
+    System.out.println("Введіть пароль:");
+    password = scanner.nextLine();
+
+    try (var fileWriter = new BufferedWriter(new FileWriter(file, append))){
+      fileWriter.append(username + "|" + password);
+      if (append){
+        fileWriter.newLine();
+      }
+    }
   }
 }
