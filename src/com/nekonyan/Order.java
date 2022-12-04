72659d00f202937652e25bc3a4b801f8dad093a4
SubarMaxKun <maxgamer0027@gmail.com>
2022-12-04 18:49:43 +0200

    Improved order creation

--- src/com/nekonyan/Order.java
+++ src/com/nekonyan/Order.java
@@ -3,6 +3,11 @@ package com.nekonyan;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Order {
@@ -19,15 +24,17 @@ public class Order {
   private void addDeliveryInfo() {
     System.out.println("Введіть ПІБ");
     fullName = "ПІБ замовника: " + scanner.nextLine();
-    System.out.println("Введіть місто та адрасе для доставки");
+    System.out.println("Введіть місто та адрасу для доставки");
     deliveryAddress = "Адреса доставки: " + scanner.nextLine();
     pasteOrder();
   }
 
   private void pasteOrder() {
+    String date = String.valueOf(LocalDate.now());
+    int orderNumber = 1000 + new Random().nextInt(9000);
     File cart = new File("./carts_orders/" + userController.getUser() + "_cart"
         + ".txt");
-    File order = new File("./carts_orders/" + userController.getUser() + "_order" + ".txt");
+    File order = new File("./carts_orders/" + userController.getUser() + "_order_" + date + "_#" + orderNumber + ".txt");
 
     try (FileInputStream fileInputStream = new FileInputStream(
         cart); FileOutputStream fileOutputStream = new FileOutputStream(order)) {
